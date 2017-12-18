def54b77ea7a7d5d48286c443e933353d02b2096
frcandela <candela.francois@gmail.com>
2017-12-18 19:04:18 +0100

    player not plocked at the bottom

--- Assets/Scripts/KeyboardController.cs
+++ Assets/Scripts/KeyboardController.cs
@@ -18,7 +18,7 @@ public class KeyboardController : Controller
 
     void FixedUpdate()
     {
-        snapInCameraView();
+        
 
         //Handle player actions if a pawn is possessed
         if (isPossessingPawn())
@@ -29,7 +29,7 @@ public class KeyboardController : Controller
             if (Input.GetButton("Fire"))
                 PossessedPawn.Fire();
 
-
+            snapInCameraView();
         }
 
 
@@ -37,22 +37,41 @@ public class KeyboardController : Controller
 
     private void snapInCameraView()
     {
+        //Calculates the width and height of the plane cutting the camera frustum
         Camera camera = GameManager.MainCamera;
         Vector3 playerPosition = PossessedPawn.transform.position;
 
         float cameraZ = Mathf.Abs(camera.transform.position.z - playerPosition.z);
-        float heightCamera = cameraZ /* Mathf.Rad2Deg*/ * Mathf.Tan(Mathf.Deg2Rad * camera.fieldOfView / 2);
+        float heightCamera = cameraZ * Mathf.Tan(Mathf.Deg2Rad * camera.fieldOfView / 2);
         float widthCamera = heightCamera * Screen.width / Screen.height;
 
+        bool vSnapped = true;
         //Vertical snap
         if (playerPosition.y < camera.transform.position.y - heightCamera)
-            PossessedPawn.transform.position = new Vector3(playerPosition.x, camera.transform.position.y - heightCamera, playerPosition.z);
+            PossessedPawn.transform.position = new Vector3(playerPosition.x, camera.transform.position.y - heightCamera + 0.1F, playerPosition.z);
         else if (playerPosition.y > camera.transform.position.y + heightCamera)
             PossessedPawn.transform.position = new Vector3(playerPosition.x, camera.transform.position.y + heightCamera, playerPosition.z);
+        else
+            vSnapped = false;
+
         //Horizontal snap
+        bool hSnapped = true;
         if (playerPosition.x < camera.transform.position.x - widthCamera)
             PossessedPawn.transform.position = new Vector3(camera.transform.position.x - widthCamera, playerPosition.y, playerPosition.z);
         else if (playerPosition.x > camera.transform.position.x + widthCamera)
             PossessedPawn.transform.position = new Vector3(camera.transform.position.x + widthCamera, playerPosition.y, playerPosition.z);
+        else
+            hSnapped = false;
+
+        /*Rigidbody2D rbPawn =  PossessedPawn.GetComponent<Rigidbody2D>();
+        if (rbPawn)
+        {
+            Vector2 newVelocity = rbPawn.velocity;
+            if (vSnapped)
+                newVelocity.y = 0;
+            if (hSnapped)
+                newVelocity.x = 0;
+            rbPawn.velocity = newVelocity;
+        }*/
     }
 }
