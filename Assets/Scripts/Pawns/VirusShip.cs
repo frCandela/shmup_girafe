f6cc16139b2a2b8847268c271efa4519435848a3
frcandela <candela.francois@gmail.com>
2018-01-02 19:34:03 +0100

    Virus mod when the player die

---
+++ Assets/Scripts/Pawns/VirusShip.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//When the player 
+public class Virus : Ship
+{
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    private void OnEnable()
+    {
+        GetComponent<SpriteRenderer>().enabled = false;
+    }
+
+    private void OnDisable()
+    {
+        GetComponent<SpriteRenderer>().enabled = false;
+    }
+}
