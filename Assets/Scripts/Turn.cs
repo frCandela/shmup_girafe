c93c0862c02f3845a699f8a93748446ca51e4e3d
Thomas Giro <thomas.giro4@etu.univ-lorraine.fr>
2018-01-20 16:31:47 +0100

    Add Tunnel Speed + Light Parameter

--- Assets/Scripts/Turn.cs
+++ Assets/Scripts/Turn.cs
@@ -5,10 +5,9 @@ using UnityEngine;
 public class Turn : MonoBehaviour {
 
     public float turnSpeed = 3f;
-    public float speed;
 
 	void Update () {
-        transform.position += new Vector3(0, speed * Time.deltaTime, 0);
+        transform.position += new Vector3(0, GameManager.instance.getTunnelSpeed() * Time.deltaTime, 0);
         if(GameManager.instance.PlayerController.PossessedPawn)
             transform.Rotate(new Vector3(0, turnSpeed * -GameManager.instance.PlayerController.PossessedPawn.transform.position.x * Time.deltaTime, 0));
 	}
