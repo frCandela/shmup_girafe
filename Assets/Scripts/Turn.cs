09a124f44c0f2c49bb4abaee9ade3b5dbd1ffd19
Thomas Giro <thomas.giro4@etu.univ-lorraine.fr>
2018-01-20 17:01:51 +0100

    Bugfix TurnScript

--- Assets/Scripts/Turn.cs
+++ Assets/Scripts/Turn.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class Turn : MonoBehaviour {
 
+    public bool isMoving = true;
     public float turnSpeed = 3f;
 
 	void Update () {
-        transform.position += new Vector3(0, GameManager.instance.getTunnelSpeed() * Time.deltaTime, 0);
+        if(isMoving)
+            transform.position += new Vector3(0, GameManager.instance.getTunnelSpeed() * Time.deltaTime, 0);
         if(GameManager.instance.PlayerController.PossessedPawn)
             transform.Rotate(new Vector3(0, turnSpeed * -GameManager.instance.PlayerController.PossessedPawn.transform.position.x * Time.deltaTime, 0));
 	}
