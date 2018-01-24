d1667a0e235b71864cdaad5d7e2b86cf1f2391c8
frcandela <candela.francois@gmail.com>
2018-01-24 10:57:43 +0100

    explosions popups(not working yet) + bugfix popups

---
+++ Assets/Scripts/Popups/ExplosionPopup.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//[RequireComponent( typeof(SpriteRenderer))]
+//[RequireComponent(typeof(Animator))]
+public class ExplosionPopup : MonoBehaviour
+{
+    Animator animator;
+    Animation anim;
+    SpriteRenderer animRenderer;
+
+    // Use this for initialization
+    void Start ()
+    {
+        /*animator = GetComponent<Animator>();
+        animRenderer = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animation>();
+
+        animRenderer.enabled = false;
+
+        if (anim)
+            print("test");
+        else
+            print("nope");*/
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if( Input.GetKeyDown( KeyCode.Space) )
+        {
+            animRenderer.enabled = true;
+            anim.Play();
+        }
+
+        //if( anim.play)
+    }
+}
