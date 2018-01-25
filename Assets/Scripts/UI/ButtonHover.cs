eb5189ea3a769f163de9d0edfde44e728d74bdf8
Thomas Giro <thomas.giro4@etu.univ-lorraine.fr>
2018-01-25 16:45:40 +0100

    UI sound

---
+++ Assets/Scripts/UI/ButtonHover.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ButtonHover : MonoBehaviour, IPointerEnterHandler {
+    
+    public FMODUnity.StudioEventEmitter emitter;
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        emitter.Play();
+    }
+
+}
