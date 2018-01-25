b78ffc7e7e074c416d9ed13a9dbf9392d6e5be38
Jonas Dutouquet <jonas.dutouquet@gmail.com>
2018-01-26 00:29:57 +0100

    Timer (need to add the GameTimer currently only in tutorial scene, TimerCanvas)

--- Assets/Scripts/UI/ScoreText.cs
+++ Assets/Scripts/UI/ScoreText.cs
@@ -7,6 +7,7 @@ using UnityEngine.UI;
 public class ScoreText : MonoBehaviour
 {
     private Text text;
+	private int currentScore = 0;
 
     void Start()
     {
@@ -15,6 +16,13 @@ public class ScoreText : MonoBehaviour
 
     public void Update()
     {
-        text.text = string.Format("{0:D10}", GameManager.instance.getScore());
+		int newScore = GameManager.instance.getScore (); 
+		if(newScore > currentScore)
+		{
+			currentScore = (int)Mathf.Lerp (currentScore, newScore, 10f * Time.deltaTime);
+			if (newScore - currentScore < 5)
+				currentScore = newScore;
+		}
+		text.text = string.Format("{0:D10}", currentScore);
     }
 }
